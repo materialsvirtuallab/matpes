585593a8cd01c4e42822cf445b8b9af17cbf71ff
Shyue Ping Ong <shyuep@gmail.com>
2024-11-19 14:19:00 -0800

    Update wioth natoms and nelements.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 """Main MatPES Explorer App."""
+
 from __future__ import annotations
 
 import collections
@@ -18,13 +19,16 @@ FUNCTIONALS = ("PBE", "r2SCAN")
 # Set up MongoDB client and database
 CLIENT = MongoClient()
 DB = CLIENT["matpes"]
-#print(DB["PBE"].find_one())
+# print(DB["PBE"].find_one())
 RAW_DATA = {}
 for f in FUNCTIONALS:
     collection = DB[f]
     RAW_DATA[f] = pd.DataFrame(
-        collection.find({}, projection=["elements", "energy", "cohesive_energy_per_atom", "formation_energy", "natoms"])
+        collection.find(
+            {}, projection=["elements", "energy", "cohesive_energy_per_atom", "formation_energy", "natoms", "nelements"]
         )
+    )
+
 
 @functools.lru_cache
 def get_data(functional, el):
@@ -34,53 +38,63 @@ def get_data(functional, el):
         df = df[df["elements"].apply(lambda x: el in x)]
     return df
 
+
 # Initialize the Dash app with a Bootstrap theme
 external_stylesheets = [dbc.themes.CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Define the app layout
-app.layout = dbc.Container([
-    dbc.Row([
-        html.Div("MatPES Explorer", className="text-primary text-center fs-3")
-    ]),
-    dbc.Row([dcc.RadioItems(
-        options=[
-            {"label": f, "value": f} for f in FUNCTIONALS
-        ],
-        value="PBE",
-        id="functional"
-    ),
-    dcc.Dropdown(
-        options=[
-            {"label": el.symbol, "value": el.symbol} for el in Element
-        ],
-        id="el_filter"
-    )]),
-    dbc.Row([
-    dcc.Graph(
-        id="ptheatmap"
-    )]),
-    dbc.Row([
-    dcc.Graph(
-        id="coh_energy_hist"
-    ),
-    dcc.Graph(
-        id="form_energy_hist"
-    )
-    ])
-])
+app.layout = dbc.Container(
+    [
+        dbc.Row([html.Div("MatPES Explorer", className="text-primary text-center fs-3")]),
+        dbc.Row(
+            [
+                html.Label("Functional"),
+                dcc.RadioItems(options=[{"label": f, "value": f} for f in FUNCTIONALS], value="PBE", id="functional"),
+                html.Label("Element Filter"),
+                dcc.Dropdown(options=[{"label": el.symbol, "value": el.symbol} for el in Element], id="el_filter"),
+            ]
+        ),
+        dbc.Row([dcc.Graph(id="ptheatmap")]),
+        dbc.Row(
+            [
+                dbc.Col([dcc.Graph(id="coh_energy_hist")], width=6),
+                dbc.Col([dcc.Graph(id="form_energy_hist")], width=6),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col([dcc.Graph(id="natoms_hist")], width=6),
+                dbc.Col([dcc.Graph(id="nelements_hist")], width=6),
+            ]
+        ),
+    ]
+)
+
 
 # Define callback to update the heatmap based on selected functional
 @callback(
-    [Output("ptheatmap", "figure"), Output("coh_energy_hist", "figure"), Output("form_energy_hist", "figure")],
-    [Input("functional", "value"), Input("el_filter", "value")]
+    [
+        Output("ptheatmap", "figure"),
+        Output("coh_energy_hist", "figure"),
+        Output("form_energy_hist", "figure"),
+        Output("natoms_hist", "figure"),
+        Output("nelements_hist", "figure"),
+    ],
+    [Input("functional", "value"), Input("el_filter", "value")],
 )
 def update_graph(functional, el_filter):
     """Update graph based on input."""
     df = get_data(functional, el_filter)
     el_count = collections.Counter(itertools.chain(*df["elements"]))
     heatmap_figure = pmv.ptable_heatmap_plotly(el_count, log=True)
-    return heatmap_figure, px.histogram(df, x="cohesive_energy_per_atom"), px.histogram(df, x="formation_energy")
+    return (
+        heatmap_figure,
+        px.histogram(df, x="cohesive_energy_per_atom"),
+        px.histogram(df, x="formation_energy"),
+        px.histogram(df, x="natoms"),
+        px.histogram(df, x="nelements"),
+    )
 
 
 # Run the app
